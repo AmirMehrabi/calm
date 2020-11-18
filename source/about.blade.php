ef67078ddff02dcc0b37b6a0d5a1c2dac332bf0b
Amir Maso'ud Mehrabian <aut0run2011@gmail.com>
2020-11-18 23:50:44 +0330

    Created an about page

---
+++ source/about.blade.php
@@ -0,0 +1,43 @@
+---
+title: درباره
+---
+@extends('_layouts.master')
+
+@section('body')
+
+<div class="container w-full mx-auto pt-20">
+    <div class="flex w-full sm:w-4/5 md:w-3/4 mx-auto">
+
+        <div class=" text-gray-700 w-full sm:w-2/3 md:w-3/4 text-center px-4 py-2 m-2 text-right">
+            <h1 class="text-2xl sm:text-3xl lg:text-4xl font-black mb-4">درباره</h1>
+            <p class="text-base sm:text-lg font-light mb-2">
+
+                این کتاب با عنوان اصلی «It Doesn't Have To Be Crazy At Work» توسط جیسون فرید و دیوید هاینمایر هانسن نوشته شده. شرکت Basecamp توسط این دو راه‌اندازی شده و دیوید خالق چارچوب نرم‌افزاری Ruby On Rails هم هست که اگر در صنعت نرم‌افزار مشغول به کار باشید حتماً
+                اسمش به گوشتان آشناست و احتمالاً در جریان باشید که توییتر، گیت‌هاب، ساوندکلود و بسیاری از نرم‌افزار دیگر بر پایهٔ همین چارچوب نرم‌افزاری توسعه یافته‌اند.
+            </p>
+
+            <p class="text-base sm:text-lg font-light mb-2">
+                قبل از این هم جیسون و دیوید کتاب‌هایی در زمینهٔ مدیریت فردی و سازمانی با عناوینی مثل ReWork، Remote و Getting Real نوشته بودند که آن‌ها هم مثل همین کتاب تبدیل به پر‌فروش‌ترین کتاب‌های سال شدند.
+
+            </p>
+
+
+            <p class="text-base sm:text-lg font-light mb-2">
+                من طرفدار فرهنگ سازمانی Basecamp هستم و دوست دارم این فرهنگ به باقی مردم هم شناسانده شود. برای همین تصمیم به ترجمهٔ این کتاب گرفتم.
+            </p>
+
+            <p class="text-base sm:text-lg font-light mb-2">
+                این کتاب از ۷ بخش و ۶۸ فصل تشکیل شده و سعی می‌کنم جمعه تا جمعه، دو سه فصل از آن را ترجمه کنم. برای ترجمهٔ عنوان کتاب هم از دوستم سارا و بهرنگ رجبی کمک گرفتم. جای ذکر دارد که این اولین ترجمهٔ بلند من است و مطمئناً دارای اشکالات فراوان، که به مرور زمان
+                سعی در رفعشان خواهم داشت.
+
+            </p>
+
+
+            <p class="text-base sm:text-lg font-light mb-2">
+
+                خواندن، معرفی و نشر این کتاب آزاد است و اتفاقاً موجب دلگرمی. :)
+            </p>
+        </div>
+    </div>
+</div>
+@endsection
