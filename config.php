3201a2e321bd3ad4d38c97522a451110dcb4011e
Amir Maso'ud Mehrabian <aut0run2011@gmail.com>
2020-11-18 23:44:25 +0330

    Updated config file

--- config.php
+++ config.php
@@ -3,7 +3,11 @@
 return [
     'production' => false,
     'baseUrl' => '',
-    'title' => 'Jigsaw',
-    'description' => 'Website description.',
-    'collections' => [],
+    'title' => 'لازه نیست زیاده‌ازحد کار کنی',
+    'description' => 'چقدر پیش آمده بشنوید یک نفر بگوید «سر کار دارم به فنا می‌رم»؟ حتی شاید خودتان هم این جمله را گفته باشید. برای خیلی‌ها، «دارم سر کار به فنا می‌رم» تبدیل به یک روتین شده. ولی دلیل این‌همه اضطراب چیست؟',
+    'collections' => [
+        'chapters' => [
+            'path' => 'chapters/{filename}'
+        ],
+    ],
 ];
