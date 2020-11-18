ba175158d86373c1ff7a58075bcff41abee936c7
Amir Maso'ud Mehrabian <aut0run2011@gmail.com>
2020-11-18 23:50:24 +0330

    Created a page for changelog

---
+++ source/changelog.blade.php
@@ -0,0 +1,65 @@
+---
+title: درباره
+---
+@extends('_layouts.master')
+
+@section('body')
+<div class="container w-full mx-auto pt-20">
+    <div class="flex w-full sm:w-4/5 md:w-3/4 mx-auto">
+
+        <div class=" text-gray-700 w-full sm:w-2/3 md:w-3/4 text-center px-4 py-2 m-2 text-right">
+            <h1 class="text-2xl sm:text-3xl lg:text-4xl font-black mb-4">تاریخچه تغییرات</h1>
+
+
+            <div class="version mb-8">
+                <h2 class="text-xl text-gray-800 font-bold border-b mb-3">نسخهٔ ۰.۳.۰ - ۲۵ اردیبهشت ۱۳۹۹</h2>
+
+                <p class="text-base sm:text-lg font-light mb-2">
+                    بکپارچه‌سازی سایز قلم‌ها در صفحات هر فصل
+                </p>
+
+                <p class="text-base sm:text-lg font-light mb-2">
+                    افزودن دکمهٔ «فصل بعد» و «فصل قبل» در صفحات هر فصل
+                </p>
+
+                <p class="text-base sm:text-lg font-light mb-2">
+                    افزودن طراحی واکنشگرا برای نمایش بهتر در دستگاه‌های کوچک‌تر
+                </p>
+
+
+            </div>
+
+
+
+            <div class="version mb-8">
+                <h2 class="text-xl text-gray-800 font-bold border-b mb-3">نسخهٔ ۰.۲.۱ - ۱۲ اردیبهشت ۱۳۹۹</h2>
+
+                <p class="text-base sm:text-lg font-light mb-2">
+                    برطرف کردن مشکل لینک‌ها در صفحهٔ نخست
+                </p>
+
+                <p class="text-base sm:text-lg font-light mb-2">
+                    یکپارچه‌سازی رنگ قلم در صفحات هر فصل
+                </p>
+
+                <p class="text-base sm:text-lg font-light mb-2">
+                    اصلاح مشکلات املایی در فصل پنجم
+                </p>
+
+
+            </div>
+
+            <div class="version mb-8">
+                <h2 class="text-xl text-gray-800 font-bold border-b mb-3">نسخهٔ ۰.۱.۰ - ۲۲ فروردین ۱۳۹۹</h2>
+
+                <p class="text-base sm:text-lg font-light mb-2">
+                    راه‌اندازی نسخهٔ نخست وب‌سایت
+                </p>
+            </div>
+
+
+            {{-- <br><br><br><br><br><br><br><br><br><br> --}}
+        </div>
+    </div>
+</div>
+@endsection
