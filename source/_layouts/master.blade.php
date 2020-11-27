8ae5db629de6e79573ea74c4c38bbe711ecc8812
Amir Maso'ud Mehrabian <aut0run2011@gmail.com>
2020-11-28 00:02:59 +0330

    Fixed some minor issues

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -19,9 +19,9 @@
     <nav id="header" class="fixed w-full z-10 top-0">
 
         <div id="progress" class="h-1 z-20 top-0" style="background:linear-gradient(to left, #553c9a var(--scroll), transparent 0);"></div>
-
+        
         <div class="w-full md:max-w-4xl mx-auto flex flex-wrap items-center justify-between mt-0 py-3">
-
+            
             <div class="pl-4">
                 <a class="text-gray-900 text-base no-underline hover:no-underline font-extrabold text-xl" href="/"> 
 					لازم نیست زیاده‌ازحد کار کنی
