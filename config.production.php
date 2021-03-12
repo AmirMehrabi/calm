755af5a0ca37e54f780e0684ddb876bdd140896b
Amir Maso'ud Mehrabian <aut0run2011@gmail.com>
2021-03-12 18:49:35 +0330

    Changed the baseUrl

--- config.production.php
+++ config.production.php
@@ -3,7 +3,7 @@
 return [
     'build' => [ 'destination' => './', ],
     'production' => true,
-    'baseUrl' => 'http://localhost/calm/build_production',
+    'baseUrl' => 'http://localhost/calm',
     'title' => 'لازه نیست زیاده‌ازحد کار کنی',
     'description' => 'چقدر پیش آمده بشنوید یک نفر بگوید «سر کار دارم به فنا می‌رم»؟ حتی شاید خودتان هم این جمله را گفته باشید. برای خیلی‌ها، «دارم سر کار به فنا می‌رم» تبدیل به یک روتین شده. ولی دلیل این‌همه اضطراب چیست؟',
     'collections' => [
